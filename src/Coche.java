43971eb1573d5a8b2994219ee6200844dd688d5e
bgomez <bgomez@Softcorpapp.com>
2023-05-09 10:22:30 -0400

    Start up

---
+++ src/Coche.java
@@ -0,0 +1,9 @@
+public class Coche {
+    int numeropuerta = 4;
+    public void setPuerta(int _puerta){
+        this.numeropuerta += _puerta;
+    }
+    public int getPuerta(){
+        return this.numeropuerta;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Coche myCoche = new Coche();
+        myCoche.setPuerta(1);
+        System.out.printf("La cantidad de puertas es: "+ myCoche.getPuerta());
+    }
+}
